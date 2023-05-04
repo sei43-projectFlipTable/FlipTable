18e2c996cfd14307953180b297b55aa5f81b4a30
zopitty <maxtan96@gmail.com>
2023-05-05 06:18:20 +0800

    created temporary NavBar and placed in linked pages

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import HomeIcon from "@mui/icons-material/Home";
@@ -7,50 +6,70 @@ import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlin
 import SearchIcon from "@mui/icons-material/Search";
 import CropFreeIcon from "@mui/icons-material/CropFree";
 import PersonIcon from "@mui/icons-material/Person";
+import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <Box>
-      <BottomNavigation
-        showLabels
-        sx={{
-          width: 430,
-          height: 50,
-          position: "absolute",
-          bottom: 0,
-          left: 0,
-          backgroundColor: "#264343",
-          borderRadius: "6px 6px 0px 0px",
-        }}
-        // value={value}
-      >
-        <BottomNavigationAction
-          sx={{ color: "#FFFFFF" }}
-          label="Home"
-          icon={<HomeIcon />}
-        />
-        <BottomNavigationAction
-          sx={{ color: "#FFFFFF" }}
-          label="Explore"
-          icon={<SearchIcon />}
-        />
-        <BottomNavigationAction
-          sx={{ color: "#FFFFFF" }}
-          label="Scan"
-          icon={<CropFreeIcon />}
-        />
-        <BottomNavigationAction
-          sx={{ color: "#FFFFFF" }}
-          label="Saved"
-          icon={<FavoriteBorderOutlinedIcon />}
-        />
-        <BottomNavigationAction
-          sx={{ color: "#FFFFFF" }}
-          label="Profile"
-          icon={<PersonIcon />}
-        />
-      </BottomNavigation>
-    </Box>
+    <BottomNavigation
+      showLabels
+      sx={{
+        width: 430,
+        height: 50,
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        backgroundColor: "#264343",
+        borderRadius: "6px 6px 0px 0px",
+      }}
+      // value={value}
+    >
+      <BottomNavigationAction
+        sx={{ color: "#FFFFFF" }}
+        label="Home"
+        icon={
+          <Link style={{ color: "white" }} to="/home">
+            <HomeIcon />
+          </Link>
+        }
+      />
+      <BottomNavigationAction
+        sx={{ color: "#FFFFFF" }}
+        label="Explore"
+        icon={
+          <Link style={{ color: "white" }} to="/explore">
+            <SearchIcon />
+          </Link>
+        }
+      />
+      <BottomNavigationAction
+        sx={{ color: "#FFFFFF" }}
+        label="Scan"
+        icon={
+          <Link style={{ color: "white" }} to="/scan">
+            <CropFreeIcon />
+          </Link>
+        }
+      />
+
+      <BottomNavigationAction
+        sx={{ color: "#FFFFFF" }}
+        label="Saved"
+        icon={
+          <Link style={{ color: "white" }} to="/saved">
+            <FavoriteBorderOutlinedIcon />
+          </Link>
+        }
+      />
+      <BottomNavigationAction
+        sx={{ color: "#FFFFFF" }}
+        label="Profile"
+        icon={
+          <Link style={{ color: "white" }} to="/profile">
+            <PersonIcon />
+          </Link>
+        }
+      />
+    </BottomNavigation>
   );
 }
 
