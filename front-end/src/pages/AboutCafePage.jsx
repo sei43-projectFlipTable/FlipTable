7c39ea86ead64f9467f9d1a12fb7086d00711fa7
SeptismJustinn <ohhoehoe@gmail.com>
2023-05-04 16:31:30 +0800

    Prepared css file for AboutPage

--- front-end/src/pages/AboutCafePage.jsx
+++ front-end/src/pages/AboutCafePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PhoneTopBar from "../components/PhoneTopBar";
+import styles from "./AboutCafePage.module.css";
 
 function AboutCafePage() {
   return (
     <>
       <PhoneTopBar />
+      <div></div>
     </>
   );
 }
