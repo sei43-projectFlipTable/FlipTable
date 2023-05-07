70a2a0253630ba6b907db7aa28ba72e8edbcd1ba
zimlow <ziminglow@gmail.com>
2023-05-07 21:34:14 +0800

    95% css for collect page
    might try MUI grid later on

--- front-end/src/pages/ScanPage.jsx
+++ front-end/src/pages/ScanPage.jsx
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 import PhoneTopBar from "../components/PhoneTopBar";
 import AppHeader from "../components/AppHeader";
 import { Link } from "react-router-dom";
-
+import HelpIcon from "@mui/icons-material/Help";
 import { Box, Button, Modal, Typography } from "@mui/material";
 import NavBar from "../components/NavBar";
+import styles from "../components/ScanBody.module.css";
+import FlashAutoIcon from "@mui/icons-material/FlashAuto";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 
 function ScanPage() {
   const [showModal, setShowModal] = useState(false);
@@ -46,19 +50,28 @@ function ScanPage() {
       </Modal>
 
       <PhoneTopBar />
-      <br />
       <AppHeader />
+      <div className={styles.mainBody}>
+        <div className={styles.collect}>Collect</div>
+        <div className={styles.helpicon}>
+          <HelpIcon /> {/*onclick =  popup for the help*/}
+        </div>
 
-      <br />
-      <div>Scan your receipt purchase to collect points!</div>
-      <br />
+        <div className={styles.instructions}>Scan your receipt purchase to collect points!</div>
+        <div className={styles.cameraView}>Camera Viewport</div>
+        <div className={styles.cameraFunctions}>
+          <button className="light-button">
+            <FlashAutoIcon />
+          </button>
+          <button className={styles.captureButton}>
+            <PhotoCameraIcon sx={{ color: "#264343", height: "26px", width: "24px" }} />
+          </button>
+          <button className="upload-photo">
+            <AddPhotoAlternateIcon />
+          </button>
+        </div>
+      </div>
 
-      <div className="camera-view">Camera Viewport</div>
-      <br />
-
-      <button className="light-button">Light</button>
-      <button className="capture-button">Capture</button>
-      <button className="upload-photo">photo</button>
       <NavBar />
     </>
   );
