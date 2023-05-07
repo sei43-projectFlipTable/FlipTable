5f07dbec57225af442a9da291e5b5b7a8c09d193
zimlow <ziminglow@gmail.com>
2023-05-07 10:59:45 +0800

    implement mui modal

--- front-end/src/pages/ScanPage.jsx
+++ front-end/src/pages/ScanPage.jsx
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
 import PhoneTopBar from "../components/PhoneTopBar";
-import ScanBarModal from "../components/ScanBarModal";
+import { Box, Button, Modal, Typography } from "@mui/material";
 
 function ScanPage() {
-  const [showOverlay, setShowOverlay] = useState(true);
+  const [showModal, setShowModal] = useState(true);
+  const handleOpen = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
+
+  const style = {
+    position: "absolute",
+    textAlign: "center",
+    top: "40%",
+    width: "100%",
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    p: 2,
+    fontSize: "1.5rem",
+  };
 
   return (
     <>
-      {showOverlay && <ScanBarModal setShowOverlay={setShowOverlay} />}
+      <Modal
+        open={showModal}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          Please Select
+          <br />
+          <Button onClick={handleClose}>Collect Points</Button>
+          <br />
+          <Button>Redeem Cash</Button>
+        </Box>
+      </Modal>
 
       <PhoneTopBar />
       <div>
