72212125ede966a6d89fe6da30f5af928b6b3f8b
SeptismJustinn <ohhoehoe@gmail.com>
2023-05-08 10:01:37 +0800

    Partial work on CafeSchema
    -Added name, address, website and priceRating to schema

--- back-end/models/Cafes.js
+++ back-end/models/Cafes.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
-const CafesSchema = new mongoose.Schema({}, { collection: "cafes" });
+const CafesSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true, minLength: 1 },
+    address: { type: String },
+    website: { type: String },
+    priceRating: { type: Number, min: 1, max: 3 },
+  },
+  { collection: "cafes" }
+);
 
 module.exports = CafesSchema;
