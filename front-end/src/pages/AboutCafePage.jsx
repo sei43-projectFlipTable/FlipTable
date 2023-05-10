ca4074b74f64bdf36a1bd79b8f0eb17c4696f924
Tan Justinn <ohhoehoe@gmail.com>
2023-05-10 23:49:32 +0800

    Pre-setup POST method for cafe info
    -Tentatively using state to retrieve cafe ID from explore/saved/home page
    -May consider putting in URL to prevent access to blank aboutcafe pages

--- front-end/src/pages/AboutCafePage.jsx
+++ front-end/src/pages/AboutCafePage.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PhoneTopBar from "../components/PhoneTopBar";
-import styles from "./AboutCafePage.module.css";
+import styles from "./css/AboutCafePage.module.css";
+import { useLocation } from "react-router-dom";
+import { fetchData } from "../helpers/common";
 
 function AboutCafePage() {
+  // Send ID from previous link over state or params?
+  const cafeId = useLocation().state?.id;
+  const [cafeData, setCafeData] = useState([]);
+
+  async function postCafe() {
+    try {
+      const { ok, data } = fetchData("/api/cafes/" + cafeId, "POST");
+      if (ok) {
+        setCafeData(data);
+      } else {
+        throw new Error(data);
+      }
+    } catch (error) {
+      console.error(error.message);
+      alert("Error getting cafe info");
+    }
+  }
+
+  useEffect(() => {
+    postCafe();
+  }, []);
+
   return (
     <>
       <PhoneTopBar />
