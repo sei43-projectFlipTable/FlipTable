7a4f730c0c116fcf6bfbcd8b67b48eb3a4706ba8
Tan Justinn <ohhoehoe@gmail.com>
2023-05-11 22:35:03 +0800

    AboutCafePage base setup
    -Added image to serve as background (z-index -1)
    -Added transparent blocker to push content boxes down
    -Added content div to display content
    -Added temporary buttons to navigate between About sections
    -Conditionally render About/Menu/Reviews according to selection.
    -AboutPage now accessed via cafeId in params

--- front-end/src/pages/AboutCafePage.jsx
+++ front-end/src/pages/AboutCafePage.jsx
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from "react";
 import PhoneTopBar from "../components/PhoneTopBar";
 import styles from "./css/AboutCafePage.module.css";
-import { useLocation } from "react-router-dom";
 import { fetchData } from "../helpers/common";
+import { useParams } from "react-router-dom";
+import AboutCafe from "../components/aboutCafePage/AboutCafe";
+import AboutCafeMenu from "../components/aboutCafePage/AboutCafeMenu";
+import AboutCafeReview from "../components/aboutCafePage/AboutCafeReview";
+import NavBar from "../components/NavBar";
 
+// CafeId in params
 function AboutCafePage() {
-  // Send ID from previous link over state or params?
-  const cafeId = useLocation().state?.id;
+  const { cafeId } = useParams();
+  // 0 - About, 1 - Menu, 2 - Reviews
   const [cafeData, setCafeData] = useState([]);
+  const [page, setPage] = useState(0);
+  const aboutPages = [
+    <AboutCafe cafeId={cafeId} cafeData={cafeData} />,
+    <AboutCafeMenu cafeId={cafeId} />,
+    <AboutCafeReview cafeId={cafeId} />,
+  ];
 
   async function postCafe() {
     try {
-      const { ok, data } = fetchData("/api/cafes/" + cafeId, "POST");
+      const { ok, data } = await fetchData("/api/cafes/" + cafeId, "POST");
       if (ok) {
         setCafeData(data);
       } else {
@@ -30,7 +41,25 @@ function AboutCafePage() {
   return (
     <>
       <PhoneTopBar />
-      <div></div>
+      <img src={cafeData.image} />
+      <div className={styles.blocker} />
+      <div className={styles.curvedTop}>
+        <div className={styles.content}>
+          <div>
+            <button className={`btn ${styles.tab}`} onClick={() => setPage(0)}>
+              About
+            </button>
+            <button className={`btn ${styles.tab}`} onClick={() => setPage(1)}>
+              Menu
+            </button>
+            <button className={`btn ${styles.tab}`} onClick={() => setPage(2)}>
+              Reviews
+            </button>
+          </div>
+          {aboutPages[page]}
+        </div>
+      </div>
+      <NavBar />
     </>
   );
 }
