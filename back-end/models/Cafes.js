bae3edc50a6bca886ccf80ee41e9d48ae7efe8ac
Tan Justinn <ohhoehoe@gmail.com>
2023-05-12 20:02:40 +0800

    Changed endpoint for cafe rating
    -Cafe ratings now stored within Cafe model, and is updated when new reviews are PUT in

--- back-end/models/Cafes.js
+++ back-end/models/Cafes.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 
+// Review rating should not be allowed manual entry from front end, calculate n update only when review added
 const CafesSchema = new mongoose.Schema(
   {
     name: { type: String, require: true, minLength: 1 },
     address: { type: String, require: true },
-    image: { type: String },
+    image: { type: Buffer },
     website: { type: String },
     priceRating: { type: Number, min: 1, max: 3 },
+    reviewRating: {
+      ratingTotal: { type: Number, min: 0, default: 0 },
+      reviewCount: { type: Number, min: 0, default: 0 },
+    },
     openingHours: { type: String },
     description: { type: String },
     tags: {
--- back-end/routers/cafes.js
+++ back-end/routers/cafes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { getCafes, putCafes, postCafes } = require("../controllers/cafes");
+const {
+  getCafes,
+  putCafes,
+  postCafes,
+  getAverageRating,
+} = require("../controllers/cafes");
 const router = express.Router();
 
 router.get("/cafes", getCafes);
 router.put("/cafes", putCafes);
 router.post("/cafes/:cafeId", postCafes);
+router.post("/cafes/rate/:cafeId", getAverageRating);
 
 module.exports = router;
