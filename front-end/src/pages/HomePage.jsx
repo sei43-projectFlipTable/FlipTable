47b949add3d5a12c6c5254dd3ea8fa6a18a5964e
Allankyaw <kyaw87.2000@gmail.com>
2023-05-13 17:25:53 +0800

    Main Page Top Part
    Fliptable, Points, Cash Value and Referral Box

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PhoneTopBar from "../components/PhoneTopBar";
 import NavBar from "../components/NavBar";
+import PointsAndCashValue from "../components/PointsAndCashValue";
 
 function HomePage() {
   return (
     <>
       <PhoneTopBar />
+      <PointsAndCashValue />
       <NavBar />
     </>
   );
