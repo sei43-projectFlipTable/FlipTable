eb2bb82b947b0bc3ca0fd528ca3be31327b37ea2
SeptismJustinn <ohhoehoe@gmail.com>
2023-05-13 12:59:45 +0800

    Seed function added

--- back-end/routers/cafes.js
+++ back-end/routers/cafes.js
@@ -5,8 +5,10 @@ const {
   postCafes,
   getAverageRating,
 } = require("../controllers/cafes");
+const { seedCafes } = require("../controllers/seed");
 const router = express.Router();
 
+router.get("/seed", seedCafes);
 router.get("/cafes", getCafes);
 router.put("/cafes", putCafes);
 router.post("/cafes/:cafeId", postCafes);
