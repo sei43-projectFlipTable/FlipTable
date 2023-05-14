bc380ec026361f8b7fdee13d2d38bb37ebe56070
Tan Justinn <ohhoehoe@gmail.com>
2023-05-14 15:43:13 +0800

    Added cafe Menu section to AboutPage

--- front-end/src/components/aboutCafePage/AboutCafeMenu.jsx
+++ front-end/src/components/aboutCafePage/AboutCafeMenu.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import styles from "../css/AboutCafeMenu.module.css";
+import MenuCard from "./MenuCard";
 
 function AboutCafeMenu(props) {
   return (
     <div>
       <h5>Menu</h5>
-      <div className={styles.menuContainer}></div>
+      <div className={styles.menuContainer}>
+        {props.cafeMenu.map((item) => {
+          return (
+            <MenuCard
+              key={item._id}
+              id={item._id}
+              name={item.name}
+              price={item.price}
+              image={item.image}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
