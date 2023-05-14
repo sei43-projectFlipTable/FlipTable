da557c0c712ac6d7704cb6f503f7ce578ec10cbf
Tan Justinn <ohhoehoe@gmail.com>
2023-05-14 21:10:36 +0800

    Removed CafeTag, no longer in use

--- front-end/src/components/CafeTag.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./css/CafeTag.module.css";
-import { amenities } from "../helpers/amenities";
-
-function CafeTag(props) {
-  return <div className={styles.cafetag}>{amenities[props.children]}</div>;
-}
-
-export default CafeTag;
