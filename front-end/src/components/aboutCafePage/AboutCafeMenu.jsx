5d2ba8f29bda604a3b30a84cfe0ac691b94f9818
Tan Justinn <ohhoehoe@gmail.com>
2023-05-14 13:28:45 +0800

    Added menu background

--- front-end/src/components/aboutCafePage/AboutCafeMenu.jsx
+++ front-end/src/components/aboutCafePage/AboutCafeMenu.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "../css/AboutCafeMenu.module.css";
 
 function AboutCafeMenu(props) {
-  return <div>AboutCafeMenu</div>;
+  return (
+    <div>
+      <h5>Menu</h5>
+      <div className={styles.menuContainer}></div>
+    </div>
+  );
 }
 
 export default AboutCafeMenu;
