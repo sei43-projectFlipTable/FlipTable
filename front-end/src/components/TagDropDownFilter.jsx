1f0a981f09980545572ee20d056f589fb8b2e3b9
Tan Justinn <ohhoehoe@gmail.com>
2023-05-14 21:06:01 +0800

    Shifted DropDown box into its own component
    -To be reused in explore/saved pages

---
+++ front-end/src/components/TagDropDownFilter.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "./css/TagDropDownFilter.module.css";
+import FilterListSharpIcon from "@mui/icons-material/FilterListSharp";
+import { amenities } from "../helpers/amenities";
+
+function TagDropDownFilter(props) {
+  return (
+    <div className={styles.tagContainer}>
+      <select
+        className={styles.tagSelector}
+        name="tag-select"
+        id="tag-select"
+        value={props.selectedTag}
+        onChange={props.handleChooseTag}
+      >
+        <option key="0" value="">
+          Search
+        </option>
+        {props.availableTags.length > 0 &&
+          props.availableTags.map((item, idx) => {
+            if (item === "withmedia") {
+              return (
+                <option key={idx + 1} value={item}>
+                  {"Cafe with Media"}
+                </option>
+              );
+            } else {
+              return (
+                <option key={idx + 1} value={item}>
+                  {"Cafe with " + amenities[item]}
+                </option>
+              );
+            }
+          })}
+      </select>
+      <FilterListSharpIcon
+        fontSize="small"
+        sx={{
+          color: "#979797",
+          position: "absolute",
+          top: "9px",
+          right: "22px",
+        }}
+      />
+    </div>
+  );
+}
+
+export default TagDropDownFilter;
--- front-end/src/components/aboutCafePage/AboutCafeReview.jsx
+++ front-end/src/components/aboutCafePage/AboutCafeReview.jsx
@@ -1,86 +1,86 @@
 import React, { useState, useEffect } from "react";
-import AddSharpIcon from "@mui/icons-material/AddSharp";
-import { Box, Modal } from "@mui/material";
 import styles from "../css/AboutCafeReview.module.css";
 import styles2 from "../css/AboutCafe.module.css";
-import CafeTag from "../CafeTag";
 import ReviewCard from "./ReviewCard";
+import TagDropDownFilter from "../TagDropDownFilter";
 
 function AboutCafeReview(props) {
-  const dbTags = ["wifi", "power", "aircon", "workspace", "lighting"];
-  const [showModal, setShowModal] = useState(false);
+  const dbTags = [
+    "wifi",
+    "power",
+    "aircon",
+    "workspace",
+    "lighting",
+    "withmedia",
+  ];
+  const [selectedTag, setSelectedTag] = useState("");
   const [availableTags, setAvailableTags] = useState(dbTags);
-  const [filteredTags, setFilteredtags] = useState([]);
 
   useEffect(() => {
     const tagArr = [];
     for (const review of props.cafeReviews) {
+      if (!tagArr.includes("withmedia") && review.image != "") {
+        // Check if at least one review has image.
+        tagArr.push("withmedia");
+      }
       for (const reviewTag of review.tags) {
         if (!tagArr.includes(reviewTag)) {
+          // Check which tags are contained within reviews
           tagArr.push(reviewTag);
         }
       }
     }
     setAvailableTags(tagArr);
   }, [props.cafeReviews]);
+
+  function selectReviews(reviewArr) {
+    if (selectedTag != "") {
+      return mapOutReviews(
+        reviewArr.filter((item) => {
+          return item.tags.includes(selectedTag);
+        })
+      );
+    } else {
+      return mapOutReviews(reviewArr);
+    }
+  }
+
+  function mapOutReviews(reviewArr) {
+    if (reviewArr.length > 0) {
+      return reviewArr.map((item, idx) => {
+        return (
+          <div key={idx}>
+            {idx != 0 && <hr />}
+            <ReviewCard
+              key={item._id}
+              id={item._id}
+              rating={item.rating}
+              image={item.image}
+            >
+              {item.review}
+            </ReviewCard>
+          </div>
+        );
+      });
+    } else {
+      return "No reviews found";
+    }
+  }
+
+  function handleChooseTag(event) {
+    setSelectedTag(event.target.value);
+  }
+
   return (
     <div>
       <h5 className={styles2.header}>Reviews</h5>
-      <div className={styles.tagContainer}>
-        {/* {availableTags.map((item, idx) => {
-          return (
-            <CafeTag key={idx} id={idx}>
-              {item}
-            </CafeTag>
-          );
-        })} */}
-        <div
-          className={styles.addTag}
-          onClick={() => {
-            setShowModal(true);
-          }}
-        >
-          <AddSharpIcon fontSize="small" />
-        </div>
-        <Modal open={showModal}>
-          <Box
-            sx={{
-              position: "absolute",
-              textAlign: "center",
-              left: "5%",
-              top: "20%",
-              width: "90%",
-              borderRadius: "6px",
-              bgcolor: "background.paper",
-              p: 2,
-              fontSize: "20px",
-              fontFamily: "Poppins",
-              color: "#264343",
-              fontWeight: 700,
-              spacing: "1px",
-              outline: 0,
-            }}
-          >
-            Choose tag to filter reviews by:
-          </Box>
-        </Modal>
-      </div>
+      <TagDropDownFilter
+        selectedTag={selectedTag}
+        handleChooseTag={handleChooseTag}
+        availableTags={availableTags}
+      />
       <div className={styles.reviewContainer}>
-        {props.cafeReviews.map((item, idx) => {
-          return (
-            <div key={idx}>
-              {idx != 0 && <hr key={`hr ${idx}`} />}
-              <ReviewCard
-                key={item._id}
-                id={item._id}
-                rating={item.rating}
-                image={item.image}
-              >
-                {item.review}
-              </ReviewCard>
-            </div>
-          );
-        })}
+        {selectReviews(props.cafeReviews)}
       </div>
     </div>
   );
