0a595c25dc7da3da45eb6667d9666d05560bf26c
Tan Justinn <ohhoehoe@gmail.com>
2023-05-14 21:31:40 +0800

    Altered user schema to save cafeIDs
    -To make it easier to populate Saved places

--- back-end/models/Users.js
+++ back-end/models/Users.js
@@ -6,11 +6,18 @@ const UsersSchema = new mongoose.Schema(
     email: { type: String, require: true },
     hash: { type: String, require: true },
     role: { type: String },
-    savedPlaces: { type: [String] },
+    savedPlaces: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Cafes",
+      default: [],
+    },
     points: { type: Number, default: 0 },
     referredCount: { type: Number },
     //returns a random string of 8 characters
-    referralCode: { type: String, default: Math.random().toString(36).substring(2, 9) },
+    referralCode: {
+      type: String,
+      default: Math.random().toString(36).substring(2, 9),
+    },
     wasReferred: { type: Boolean },
   },
   { collection: "users" }
