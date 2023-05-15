6770e97ebf8874e820eba135bbb54c772803180e
SeptismJustinn <ohhoehoe@gmail.com>
2023-05-15 15:55:57 +0800

    Explore and Saved page main display done
    -Added display for unfiltered cafes

--- front-end/src/pages/ExplorePage.jsx
+++ front-end/src/pages/ExplorePage.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PhoneTopBar from "../components/PhoneTopBar";
 import NavBar from "../components/NavBar";
 import AppHeader from "../components/AppHeader";
+import { fetchData } from "../helpers/common";
+import SaveExploreDisplay from "../components/SaveExploreDisplay";
 
 function ExplorePage() {
+  const [cafeData, setCafeData] = useState([]);
+
+  // GET all cafes
+  async function getCafes() {
+    try {
+      const { ok, data } = await fetchData("/api/cafes");
+      if (ok) {
+        setCafeData(data);
+      } else {
+        throw new Error(data);
+      }
+    } catch (error) {
+      alert("Error getting cafes");
+    }
+  }
+
+  useEffect(() => {
+    getCafes();
+  }, []);
+
   return (
     <>
       <PhoneTopBar />
       <AppHeader />
+      <SaveExploreDisplay cafeData={cafeData}>
+        Featured Cafes
+      </SaveExploreDisplay>
       <NavBar />
     </>
   );
--- front-end/src/pages/SavedCafesPage.jsx
+++ front-end/src/pages/SavedCafesPage.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PhoneTopBar from "../components/PhoneTopBar";
 import NavBar from "../components/NavBar";
 import AppHeader from "../components/AppHeader";
+import { fetchData } from "../helpers/common";
+import SaveExploreDisplay from "../components/SaveExploreDisplay";
 
 function SavedCafesPage() {
+  const [cafeData, setCafeData] = useState([]);
+
+  // GET all user cafes
+  async function getUserCafes() {
+    try {
+      const { ok, data } = await fetchData("/api/cafes");
+      if (ok) {
+        setCafeData(data);
+      } else {
+        throw new Error(data);
+      }
+    } catch (error) {
+      alert("Error getting cafes");
+    }
+  }
+
+  useEffect(() => {
+    getUserCafes();
+  }, []);
+
   return (
     <>
       <PhoneTopBar />
       <AppHeader />
+      <SaveExploreDisplay cafeData={cafeData}>Saved Cafes</SaveExploreDisplay>
       <NavBar />
     </>
   );
