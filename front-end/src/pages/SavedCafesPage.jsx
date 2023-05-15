1f491820166ac653e832196f938375b08607ee6d
SeptismJustinn <ohhoehoe@gmail.com>
2023-05-15 14:14:51 +0800

    Added AppHeader to pages
    -Renamed AppHeader classname for consistency

--- front-end/src/pages/SavedCafesPage.jsx
+++ front-end/src/pages/SavedCafesPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PhoneTopBar from "../components/PhoneTopBar";
 import NavBar from "../components/NavBar";
+import AppHeader from "../components/AppHeader";
 
 function SavedCafesPage() {
   return (
     <>
       <PhoneTopBar />
+      <AppHeader />
       <NavBar />
     </>
   );
