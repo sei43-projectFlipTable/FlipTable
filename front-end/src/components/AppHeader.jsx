903773e93f72d9fb593b9a36dfa8f5a88d55c0eb
Tan Justinn <ohhoehoe@gmail.com>
2023-05-17 20:47:30 +0800

    Logout cheatcode in app header
    -Click 10 times to enable logout

--- front-end/src/components/AppHeader.jsx
+++ front-end/src/components/AppHeader.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./css/AppHeader.module.css";
 
 function AppHeader() {
-  return <div className={styles.appHeader}>FlipTable</div>;
+  const [counter, setCounter] = useState(0);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.clear();
+    setCounter(0);
+    navigate("/");
+  }
+
+  if (counter < 10) {
+    return (
+      <div
+        className={styles.appHeader}
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        FlipTable
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.appHeader} onClick={handleLogout}>
+        Logout
+      </div>
+    );
+  }
 }
 
 export default AppHeader;
