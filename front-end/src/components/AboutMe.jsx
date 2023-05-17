e029ec34166f757b4dc41f809d5edb90395c6baf
Allankyaw <kyaw87.2000@gmail.com>
2023-05-17 15:28:22 +0800

    Completed Profile page with username and aboutme

--- front-end/src/components/AboutMe.jsx
+++ front-end/src/components/AboutMe.jsx
@@ -1,47 +1,76 @@
 import { Box } from "@mui/material";
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import styles from "./css/AboutMe.module.css";
-import { Link } from "react-router-dom";
+import { fetchData } from "../helpers/common";
+import UserContext from "../context/user";
 
 const AboutMe = () => {
-  //   const [aboutMe, setAboutMe] = useState("Write about yourself here!");
-  const [editing, setEditing] = useState(true);
-  const [displayText, setDisplayText] = useState("Write about yourself!");
+  const [editing, setEditing] = useState(false);
   const [inputText, setInputText] = useState("");
-  //reverse the true and false for better clarity
-  const updateAll = () => {
-    setDisplayText(inputText);
+  const userCtx = useContext(UserContext);
+  const [userBio, setUserBio] = useState();
+
+  const getUserBio = async () => {
+    try {
+      const { ok, data } = await fetchData(
+        "/user",
+        userCtx.accessToken,
+        "POST",
+        {
+          id: userCtx.payload.id,
+        }
+      );
+
+      if (ok) {
+        setUserBio(data.aboutMe);
+      } else throw new Error(data);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const updateUserBio = async () => {
+    try {
+      const { ok, data } = await fetchData(
+        "/user",
+        userCtx.accessToken,
+        "PATCH",
+        {
+          id: userCtx.payload.id,
+          aboutMe: inputText,
+        }
+      );
+
+      if (ok) {
+        alert("About me updated");
+        setEditing(false);
+        getUserBio();
+        setInputText();
+      } else {
+        throw new Error(data);
+      }
+    } catch (error) {
+      alert("error updating about me");
+    }
+  };
+
+  const openUpdateBox = () => {
     setEditing(true);
   };
 
-  const updateEditFalse = () => {
+  const closeUpdateBox = () => {
     setEditing(false);
+    setInputText();
   };
 
-  const updateEditTrue = () => {
-    setEditing(true);
-    setInputText("");
-  };
+  useEffect(() => {
+    getUserBio();
+  }, [userCtx.accessToken]);
+
   return (
     <>
       <div className={styles.homePageScroll}>
         {editing ? (
-          <div className={styles.referralComponentSpace}>
-            <div className={styles.referralHeaderSpace}>
-              <div className={styles.referralHeaderTextSpace}>
-                <div className={styles.referralHeaderText}>About Me</div>
-              </div>
-            </div>
-            <div onClick={updateEditFalse} className={styles.referralBodySpace}>
-              <div className={styles.referralBodyDescriptionSpace}>
-                <div className={styles.referralBodyDescription}>
-                  {displayText}
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : (
           <div>
             <div className={styles.referralComponentSpace}>
               <div className={styles.referralHeaderSpace}>
@@ -55,33 +84,47 @@ const AboutMe = () => {
                     <input
                       className={styles.noborder}
                       style={{
-                        width:
-                          Math.min(Math.max(inputText.length, 35), 35) + "ch",
+                        width: 35 + "ch",
                         //this is how u control the textbox width, u can just put 35
                       }}
                       type="text"
-                      value={inputText}
                       onChange={(e) => setInputText(e.target.value)}
                       placeholder="Write your bio here!"
-                      maxLength="200"
+                      // maxLength="200"
                     />
                   </div>
                 </div>
                 <div className={styles.aboutMeButton}>
                   <div className={styles.referralButtonSpace}>
-                    <div onClick={updateAll} className={styles.referralButton}>
+                    <div
+                      onClick={updateUserBio}
+                      className={styles.referralButton}
+                    >
                       Update
                     </div>
                   </div>
                   <div className={styles.referralButtonSpace}>
                     <div
-                      onClick={updateEditTrue}
+                      onClick={closeUpdateBox}
                       className={styles.referralButton}
                     >
                       Cancel
                     </div>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className={styles.referralComponentSpace}>
+            <div className={styles.referralHeaderSpace}>
+              <div className={styles.referralHeaderTextSpace}>
+                <div className={styles.referralHeaderText}>About Me</div>
+              </div>
+            </div>
+            <div onClick={openUpdateBox} className={styles.referralBodySpace}>
+              <div className={styles.referralBodyDescriptionSpace}>
+                <div className={styles.referralBodyDescription}>{userBio}</div>
               </div>
             </div>
           </div>
